b0b859a1b7b5a8d3c88cac4338a8d43d35b07ace
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-09-08 00:59:17 +0700

    no. 4

--- scripts/trim_word.py
+++ scripts/trim_word.py
@@ -1,10 +1,10 @@
 '''Trim Word'''
 import unittest
 
-def trim_text(text:str, len:int):
-    if type(text) != str or type(len) != int:
+def trim_text(text:str, len_text:int):
+    if type(text) != str or type(len_text) != int:
         raise ValueError
-    trim = ' '.join(text.split(" ")[:len])
+    trim = ' '.join(text.split(" ")[:len_text])
     return trim
 
 # print(trim_text("satu dua tiga empat lima", 2))
